46daf2ff5e00495c83b14bf434203e95ba0b3398
icburrito <icburrito6@gmail.com>
2020-06-01 20:43:42 -0300

    NEWS update

--- px/src/app/app-routing.module.ts
+++ px/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { NewsComponent } from './components/news/news.component';
+import { GamesComponent } from './components/games/games.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
@@ -13,6 +14,7 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
   {path: 'news', component: NewsComponent},
+  {path: 'games', component: GamesComponent},
   {path: 'projects', component: ProjectsComponent},
   {path: 'about', component: AboutComponent},
   {path: 'contact', component: ContactComponent},
--- px/src/app/app.module.ts
+++ px/src/app/app.module.ts
@@ -3,18 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 import { NewsComponent } from './components/news/news.component';
+import { GamesComponent } from './components/games/games.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { HomeComponent } from './components/home/home.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NewsComponent,
+    GamesComponent,
     ProjectsComponent,
     AboutComponent,
     ContactComponent,
